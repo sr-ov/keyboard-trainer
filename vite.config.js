184536c68e69967cedcaab6c4dd387f687742039
sr-ov <c8@yandex.ru>
2022-12-05 16:35:42 +0400

    added base to config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+	base: '/keyboard-trainer/',
 	root: resolve(__dirname, 'src'),
 	build: {
 		rollupOptions: {
